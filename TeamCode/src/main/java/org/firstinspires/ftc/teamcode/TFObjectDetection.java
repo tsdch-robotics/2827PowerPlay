93c43dbf64ed9457c95443730d741427044ce7c1
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2023-01-03 11:31:58 -0500

    Update TFObjectDetection.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFObjectDetection.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
