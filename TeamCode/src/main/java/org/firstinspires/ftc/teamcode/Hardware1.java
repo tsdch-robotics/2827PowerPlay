a822f7bc1ddbf6383ddb9cd0eb9d5b29b1bf62ca
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2023-01-20 12:01:56 -0500

    Update Hardware1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware1.java
@@ -30,6 +30,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
