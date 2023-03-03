1d2fd02072dccdf7760b7856e5f8b6518ec7aead
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2023-03-03 09:31:01 -0500

    Create Hannahteleop.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hannahteleop.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+//idk what any of this is or if i need or not but i copied and pasted from OutreachBot;
+
+
+import org.firstinspires.ftc.robotcontroller.external.samples.BasicOmniOpMode_Linear;
+
+
+@TeleOp()
+public class Hannahteleop extends  {
+    public void init() {
+        double motorSpeed = 0.5;
+
+
+    }
+}
+
+
+
