3c95401100892561e45c9517daf7bba0a07fb3da
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2023-03-06 09:42:06 -0500

    Update Hannahteleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hannahteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hannahteleop.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.Nullable;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -14,9 +17,18 @@ import org.firstinspires.ftc.robotcontroller.external.samples.BasicOmniOpMode_Li
 
 
 @TeleOp()
-public class Hannahteleop extends  {
+public class Hannahteleop extends {
+    Hardware1 robot = new Hardware1();
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+}
     public void init() {
         double motorSpeed = 0.5;
+        robot.init(Hardware1);
+
 
 
     }
